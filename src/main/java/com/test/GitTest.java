bc45a02bf24e26d1429f054824ff2124dfc263a4
nfx <xiaoyi19n@126.com>
2023-10-23 21:45:06 +0800

    first commit

---
+++ src/main/java/com/test/GitTest.java
@@ -0,0 +1,18 @@
+package com.test;
+
+/**
+ * @Author nfx
+ * @Create 2023/10/23
+ * Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i< 10; i++){
+            System.out.println(i);
+        }
+
+    }
+
+}
